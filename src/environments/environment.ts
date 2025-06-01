a7eb92a3595821c7879cc7c830f0800d95282776
Migue025 <miguelcantalejo@live.com>
2025-06-01 14:39:35 +0200

    Se ha Dockerizado el Frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,14 @@
+// // src/environments/environment.ts
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://localhost:8080/api'
+// };
+
+
+// URL para Prod
+
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api'
+  apiBaseUrl: '/api'
 };
